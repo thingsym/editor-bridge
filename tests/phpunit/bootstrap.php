510c14a9fffbd81f5c4fb38849fa47ebd899409b
thingsym <thingsym@gmail.com>
2020-10-21 17:22:41 +0900

    test: build test environment

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -23,7 +23,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/../custom-post-type-widget-blocks.php';
+	require dirname( dirname( __FILE__ ) ) . '/../guten-plus.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 

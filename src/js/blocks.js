988b4d903b5f3e49bc97e80b567d8ee8df3539f2
thingsym <thingsym@gmail.com>
2021-09-17 16:29:10 +0900

    feat: add width settings expansion

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -6,6 +6,7 @@ import '../blocks/expansion/space/index.js';
 import '../blocks/expansion/container/index.js';
 import '../blocks/expansion/background-image/index.js';
 import '../blocks/expansion/border/index.js';
+import '../blocks/expansion/width/index.js';
 
 // style
 import '../blocks/style/heading.js';

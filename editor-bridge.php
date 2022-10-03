bd233aeba5912c2e1d6a99b79ca4525c9c3019eb
thingsym <thingsym@gmail.com>
2022-10-03 16:41:00 +0900

    chore: fix license

--- editor-bridge.php
+++ editor-bridge.php
@@ -6,7 +6,7 @@
  * Version: 1.7.0
  * Author: thingsym
  * Author URI:  https://www.thingslabo.com/
- * License:     GPL2 or later
+ * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: editor-bridge
  * Domain Path: /languages
--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ Stable tag: 1.7.0
 Tested up to: 6.0.0
 Requires at least: 5.5
 Requires PHP: 7.1
-License: GPL2 or later
+License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Tags: block, block editor, gutenberg
 

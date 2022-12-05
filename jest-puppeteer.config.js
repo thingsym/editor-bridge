fb058dcf1532086f2a9a065039eb2e722f482eae
thingsym <thingsym@gmail.com>
2022-12-05 17:47:45 +0900

    chore: fix jest config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,13 +4,24 @@ module.exports = {
 	...config,
 	rootDir: './',
 	preset: 'jest-puppeteer',
+	setupFilesAfterEnv: [
+		'<rootDir>/tests/e2e/bootstrap.js',
+		'@wordpress/jest-console',
+		'@wordpress/jest-puppeteer-axe',
+	],
 	testMatch: [
 		'<rootDir>/tests/e2e/*.test.js?(x)',
 		'<rootDir>/src/blocks/**/tests/e2e/*.test.js?(x)',
+		'<rootDir>/src/helper/**/tests/e2e/*.test.js?(x)',
 	],
 	testPathIgnorePatterns: [
 		'<rootDir>/.git/',
 		'<rootDir>/node_modules/',
 		'<rootDir>/vendor/',
-	]
+	],
+  launch: {
+    dumpio: true,
+    headless: process.env.HEADLESS !== 'false',
+    product: 'chrome',
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,12 @@ const config = require( '@wordpress/scripts/config/jest-unit.config.js' );
 module.exports = {
 	...config,
 	rootDir: './',
+	preset: '@wordpress/jest-preset-default',
 	silent: false,
 	testMatch: [
 		'<rootDir>/tests/jest/*.test.js?(x)',
 		'<rootDir>/src/blocks/**/jest/*.test.js?(x)',
+		'<rootDir>/src/helper/**/jest/*.test.js?(x)',
 	],
 	testPathIgnorePatterns: [
 		'<rootDir>/.git/',

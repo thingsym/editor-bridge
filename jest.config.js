eb0f96586ecff5492f7554a2de2591569637bf9f
thingsym <thingsym@gmail.com>
2023-04-21 15:01:11 +0900

    test: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -14,5 +14,10 @@ module.exports = {
 		'<rootDir>/.git/',
 		'<rootDir>/node_modules/',
 		'<rootDir>/vendor/',
-	]
+	],
+  testEnvironment: 'jsdom',
+	moduleNameMapper: {
+		// Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    "uuid": require.resolve('uuid'),
+  }
 };
